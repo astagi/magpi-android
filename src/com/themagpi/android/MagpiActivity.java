58afa1fc93467d395c0799ef7315ff6e89c7229c
Andrea Stagi <stagi.andrea@gmail.com>
2013-01-06 23:56:27 +0100

    Fragments structure added

--- src/com/themagpi/android/MagpiActivity.java
+++ src/com/themagpi/android/MagpiActivity.java
@@ -1,22 +1,59 @@
 package com.themagpi.android;
 
+
+
 import android.os.Bundle;
-import android.app.Activity;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentTransaction;
 import android.view.Menu;
 
-public class MagpiActivity extends Activity {
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_magpi);
-    }
+public class MagpiActivity extends FragmentActivity 
+	implements HeadlinesFragment.OnHeadlineSelectedListener {
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.activity_magpi, menu);
         return true;
     }
-    
+	
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.news_articles);
+
+        if (findViewById(R.id.fragment_container) != null) {
+
+            if (savedInstanceState != null) {
+                return;
+            }
+
+            HeadlinesFragment firstFragment = new HeadlinesFragment();
+
+            firstFragment.setArguments(getIntent().getExtras());
+
+            getSupportFragmentManager().beginTransaction()
+                    .add(R.id.fragment_container, firstFragment).commit();
+        }
+    }
+
+    public void onArticleSelected(int position) {
+
+        IssueFragment articleFrag = (IssueFragment)
+                getSupportFragmentManager().findFragmentById(R.id.issue_fragment);
+
+        if (articleFrag != null) {
+            articleFrag.updateIssueView(position);
+        } else {
+            IssueFragment newFragment = new IssueFragment();
+            Bundle args = new Bundle();
+            args.putInt(IssueFragment.ARG_POSITION, position);
+            newFragment.setArguments(args);
+            FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+
+            transaction.replace(R.id.fragment_container, newFragment);
+            transaction.addToBackStack(null);
+
+            transaction.commit();
+        }
+    }
 }
