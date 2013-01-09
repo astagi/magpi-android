45f7e98e80acbe20e9b5575148abbc34a40dd322
Andrea Stagi <stagi.andrea@gmail.com>
2013-01-09 16:39:38 +0100

    Minfixes

--- src/com/themagpi/api/MagPiClient.java
+++ src/com/themagpi/api/MagPiClient.java
@@ -3,22 +3,18 @@ package com.themagpi.api;
 import java.util.ArrayList;
 
 import org.mcsoxford.rss.RSSFeed;
-import org.mcsoxford.rss.RSSItem;
 import org.mcsoxford.rss.RSSReader;
-
-import android.util.Log;
 
 import com.loopj.android.http.AsyncHttpClient;
 import com.loopj.android.http.AsyncHttpResponseHandler;
 
 public class MagPiClient {
     
-    private static final String BASE_URL = "http://api.twitter.com/1/";
-
     private AsyncHttpClient client = new AsyncHttpClient();
 
     public static class OnIssuesReceivedListener {
         public void onReceived(ArrayList<Issue> issues) {}
+        public void onError() {}
     }
     
     public void getIssues(final OnIssuesReceivedListener issueListener) {
