71da9874011478cdcf6868656bdb52cba7132f8d
Andrea Stagi <stagi.andrea@gmail.com>
2013-12-04 18:21:34 +0100

    Update Config.java

--- src/com/themagpi/android/Config.java
+++ src/com/themagpi/android/Config.java
@@ -1,6 +1,7 @@
 package com.themagpi.android;
 
 public class Config {
+    public static final String USER = "TheMagPiBetaUser";
     public static final String APP_SECRET = "betasecret";
     public static final String SENDER_ID = "17516895224";
     public static final String SERVICE_URL = "http://192.168.0.6:12080";
