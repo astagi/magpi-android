d2ea208f8bfcdd2113b1069bba96f507874394f4
Andrea Stagi <stagi.andrea@gmail.com>
2013-12-11 04:03:54 +0100

    New news section, refactoring, minfixes

--- src/com/themagpi/adapters/LinkedAdapter.java
+++ src/com/themagpi/adapters/LinkedAdapter.java
@@ -1,39 +1,79 @@
 package com.themagpi.adapters;
 
 import java.util.ArrayList;
-import java.util.HashMap;
 
 import android.content.Context;
-import android.text.method.LinkMovementMethod;
-import android.text.util.Linkify;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.SimpleAdapter;
+import android.webkit.WebView;
+import android.widget.BaseAdapter;
 import android.widget.TextView;
 
-public class LinkedAdapter extends SimpleAdapter {
-    
-    private int[] textViews;
+import com.themagpi.android.R;
+import com.themagpi.api.News;
 
-    public LinkedAdapter(Context context, ArrayList<HashMap<String, Object>> list, int resource, String[] from, int[] to, int[] textViews) {
-        super(context, list, resource, from, to);
-        this.textViews = textViews;
+public class LinkedAdapter extends BaseAdapter {
+    
+    private class ViewHolder {
+        public WebView webView;
+        public TextView  textView;
+    }
+    
+    private ArrayList<News> news;
+    private LayoutInflater  inflater;
+    
+    public LinkedAdapter(Context context, ArrayList<News> news) {
+    	inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+    	this.news = news;
+    }
+
+    @Override
+    public int getCount() {
+        if (news != null)
+            return news.size();
+        return 0;
+    }
+
+    @Override
+    public Object getItem(int position) {
+        if (news != null && position >= 0 && position < getCount()) {
+            return news.get(position);
+        }
+        
+        return null;
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return position;
     }
 
     @Override
     public View getView(int position, View convertView, ViewGroup parent) {
-        View view = super.getView(position, convertView, parent);
-        TextView noteView = null;
-        for(int textViewId : textViews) {
-            noteView = (TextView) view.findViewById(textViewId);
-            if(noteView != null) {
-                noteView.setAutoLinkMask(Linkify.ALL);
-                noteView.setLinksClickable(true);
-                noteView.setMovementMethod(LinkMovementMethod.getInstance());
-            }
+        
+        View view = convertView;
+        ViewHolder viewHolder;
+
+        News newsModel = news.get(position);
+        
+        if (view == null) {
+            view = inflater.inflate(R.layout.adapter_news_item, parent, false);
+            
+            viewHolder = new ViewHolder();
+            viewHolder.webView = (WebView) view.findViewById(R.id.news_content);
+            viewHolder.textView  = (TextView) view.findViewById(R.id.news_date);
+            
+            view.setTag(viewHolder);
+
+        } else {
+            viewHolder = (ViewHolder) view.getTag();
         }
+        
+        viewHolder.webView.loadData(newsModel.getContent(), "text/html; charset=utf-8", "utf-8");
+        viewHolder.textView.setText(newsModel.getDate());
+        
         return view;
     }
-    
 
 }
--- src/com/themagpi/fragments/NewsFragment.java
+++ src/com/themagpi/fragments/NewsFragment.java
@@ -1,19 +1,13 @@
 package com.themagpi.fragments;
 
 import java.util.ArrayList;
-import java.util.HashMap;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.text.Html;
-import android.text.TextUtils;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.AdapterView;
-import android.widget.AdapterView.OnItemLongClickListener;
+import android.widget.BaseAdapter;
 import android.widget.ListView;
-import android.widget.SimpleAdapter;
 
 import com.actionbarsherlock.app.SherlockFragment;
 import com.actionbarsherlock.app.SherlockFragmentActivity;
@@ -71,19 +65,6 @@ public class NewsFragment extends SherlockFragment implements Refreshable {
                 getListView().setAdapter(createNewsAdapter(news));
                 ((RefreshableContainer) getActivity()).stopRefreshIndicator();
                 getActivity().findViewById(R.id.progress_news).setVisibility(View.GONE);
-                getListView().setOnItemLongClickListener(new OnItemLongClickListener() {
-
-                    @Override
-                    public boolean onItemLongClick(AdapterView<?> arg0, View arg1,
-                            int position, long arg3) {
-                        
-                        Intent shareIntent = new Intent(Intent.ACTION_SEND);
-                        shareIntent.setType("text/plain");
-                        shareIntent.putExtra(Intent.EXTRA_TEXT, NewsFragment.this.news.get(position).getContent());
-                        startActivity(Intent.createChooser(shareIntent, getResources().getString(R.string.share_news)));
-                        return true;
-                    }
-                });
             }
             public void onError(int error) {
                 ((RefreshableContainer) getActivity()).stopRefreshIndicator();
@@ -92,24 +73,8 @@ public class NewsFragment extends SherlockFragment implements Refreshable {
         });        
     }
     
-    public SimpleAdapter createNewsAdapter(ArrayList<News> news) {
-        ArrayList<HashMap<String,Object>> list_populate = new ArrayList<HashMap<String,Object>>();
-        
-        for( News pnews : news ) {     
-            HashMap<String,Object> temp = new HashMap<String,Object>();
-            String content = pnews.getContent().replaceAll("<img.+?>", "");
-            if(TextUtils.isEmpty(content))
-            	continue;
-            temp.put("news_content", Html.fromHtml(content));
-            temp.put("news_date", pnews.getDate());
-            list_populate.add(temp);
-        }
-                        
-        LinkedAdapter adapter = new LinkedAdapter(getActivity(), list_populate,
-                R.layout.adapter_news_item,
-                new String[] {"news_content", "news_date"},
-                new int[] { R.id.news_content, R.id.news_date},
-                new int[] { R.id.news_content});
+    public BaseAdapter createNewsAdapter(ArrayList<News> news) {                        
+        LinkedAdapter adapter = new LinkedAdapter(getActivity(), news);
         return adapter;
     }
 }
